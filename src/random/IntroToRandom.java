e5e9be411349026a590c20930f8bcd6e634396a8
Jessup.Jacob <Jessup.Jacob@SEMS-259-16.SEMS259.LAB>
2018-01-17 14:54:52 -0800

    IntroToRandom

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -11,20 +11,24 @@ import java.util.Random;
 
 public class IntroToRandom {
 	public static void main(String[] args) {
-		int r = 0;
+		int r = 9002;
 		
 		//2. Now make r random. 
 		//   Create an object of the Random class
 		//   Hint: new Random().nextInt()
-
+Random Anime=new Random();	
+		
 		//3. Limit the random number between 0 and 100
+r=Anime.nextInt(101);
 
 		//4. Limit the random number between 25 and 75. Hint: (highest value - lowest value) + lowest value
-
+r=Anime.nextInt((75-25)+1)+25;
 		//5. Challenge: Limit the random number between -222 and 88
 
 
 		
 		System.out.println(r); 
 	}
-}
+
+	
+	}
